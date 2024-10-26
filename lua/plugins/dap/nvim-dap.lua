e3ff5000abfba7e59152ba960f1c7ac5eb7b3772
Romain Seailles <romain.seailles@inria.fr>
2024-10-26 11:48:06 +0200

    Improve debugging

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -1,12 +1,37 @@
 return {
-  "mfussenegger/nvim-dap",
-  config = function ()
-    local dap = require("dap")
-    vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = "Toogle a breakbpoint"})
-    vim.keymap.set('n', '<leader>dc', function() dap.continue() end, { desc = "Start/continue a debugging session"})
-    vim.keymap.set('n', '<leader>do', function() dap.step_over() end, { desc = "Step over" })
-    vim.keymap.set('n', '<leader>di', function() dap.step_into() end, { desc = "Step into"})
-    vim.keymap.set('n', '<leader>dp', function() dap.step_out() end, { desc = "Step out"})
-    vim.keymap.set('n', '<leader>dx', function() dap.terminate() end, { desc = "Terminate debugging session"})
-  end,
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+
+		-- keymaps
+		local km = vim.keymap -- for conciseness
+		km.set("n", "<leader>db", function()
+			dap.toggle_breakpoint()
+		end, { desc = "Toogle a breakbpoint" })
+		km.set("n", "<leader>dc", function()
+			dap.continue()
+		end, { desc = "Start/continue a debugging session" })
+		km.set("n", "<leader>do", function()
+			dap.step_over()
+		end, { desc = "Step over" })
+		km.set("n", "<leader>di", function()
+			dap.step_into()
+		end, { desc = "Step into" })
+		km.set("n", "<leader>dp", function()
+			dap.step_out()
+		end, { desc = "Step out" })
+		km.set("n", "<leader>dx", function()
+			dap.terminate()
+		end, { desc = "Terminate debugging session" })
+
+		-- ui
+		vim.fn.sign_define(
+			"DapBreakpoint",
+			{ text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		-- vim.fn.sign_define(
+		-- 	"DapStopped",
+		-- 	{ text = "➡️", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+		-- )
+	end,
 }
