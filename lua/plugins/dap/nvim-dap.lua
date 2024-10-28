e6e4fd22916694cbf2f7e9733300d634ff5c8621
Romain Seailles <romain.seailles@inria.fr>
2024-10-28 11:21:00 +0100

    Config nvim-dap

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -7,19 +7,32 @@ return {
 		local km = vim.keymap -- for conciseness
 		km.set("n", "<leader>db", function()
 			dap.toggle_breakpoint()
-		end, { desc = "Toogle a breakbpoint" })
+		end, { desc = "Toogle a breakpoint" })
+
+		km.set("n", "<leader>dB", function()
+			dap.clear_breakpoints()
+		end, { desc = "Clear all breakpoint(s)" })
+
 		km.set("n", "<leader>dc", function()
 			dap.continue()
 		end, { desc = "Start/continue a debugging session" })
+
+		km.set("n", "<leader>dr", function()
+			dap.restart()
+		end, { desc = "Restart debugging session" })
+
 		km.set("n", "<leader>do", function()
 			dap.step_over()
 		end, { desc = "Step over" })
+
 		km.set("n", "<leader>di", function()
 			dap.step_into()
 		end, { desc = "Step into" })
+
 		km.set("n", "<leader>dp", function()
 			dap.step_out()
 		end, { desc = "Step out" })
+
 		km.set("n", "<leader>dx", function()
 			dap.terminate()
 		end, { desc = "Terminate debugging session" })
@@ -29,9 +42,5 @@ return {
 			"DapBreakpoint",
 			{ text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
 		)
-		-- vim.fn.sign_define(
-		-- 	"DapStopped",
-		-- 	{ text = "➡️", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
-		-- )
 	end,
 }
