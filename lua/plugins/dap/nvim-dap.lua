6d8575af8c354cc3e82da18a0cb1a5fe403a7c18
Romain Seailles <romain.seailles@inria.fr>
2024-10-30 11:18:45 +0100

    Add python configuration and add restart prev-configuration keybinding

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -13,6 +13,10 @@ return {
 			dap.clear_breakpoints()
 		end, { desc = "Clear all breakpoint(s)" })
 
+		km.set("n", "<leader>dC", function()
+			dap.run_last()
+		end, { desc = "Re-runs the last debug configurations than previously ran." })
+
 		km.set("n", "<leader>dc", function()
 			dap.continue()
 		end, { desc = "Start/continue a debugging session" })
@@ -42,5 +46,54 @@ return {
 			"DapBreakpoint",
 			{ text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
 		)
+
+		-- tools
+		-- Escape special characters in Lua patterns: ( ) . % + - * ? [ ^ $
+		local function escape_pattern(text)
+			return text:gsub("([%-%.%(%)%+%*%?%[%^%$])", "%%%1")
+		end
+
+		-- returns the path of the current file formated as a python module path
+		local function get_module_path()
+			local file_path = vim.fn.expand("%:p")
+			local cwd = vim.fn.getcwd()
+			local cwd_escaped = escape_pattern(cwd)
+			-- vim.notify("Full current file path is " .. file_path)
+			-- vim.notify("Current working directory is " .. cwd)
+			if not file_path:find("^" .. cwd_escaped) then
+				error(file_path .. " is not within the current working directory: " .. cwd)
+			end
+			if not file_path:match("%.py$") then
+				error(file_path .. " does not have a .py extension")
+			end
+			local relative_path = file_path:sub(#cwd + 2) -- +2 to remove both `/` and the cwd
+			local module_path = relative_path:gsub("/", "."):gsub("%.py$", "")
+			vim.notify("Debugging module " .. module_path, vim.log.levels.INFO)
+			return module_path
+		end
+
+		-- returns a python interpreter path
+		local function get_python_interpeter_path()
+			local venv = os.getenv("VIRTUAL_ENV")
+			if venv then
+				return venv .. "/bin/python"
+			else
+				vim.notify("VIRTUAL_ENV not set. Using system Python.", vim.log.levels.WARN)
+				return "/usr/bin/python3"
+			end
+		end
+
+		-- configurations
+		dap.configurations.python = {
+			{
+				type = "python",
+				request = "launch",
+				name = "Launch current module",
+				-- debugpy's python
+				-- cwd = "/path/to/repository/root/dir",
+				module = get_module_path,
+				pythonPath = get_python_interpeter_path(),
+			},
+		}
 	end,
 }
