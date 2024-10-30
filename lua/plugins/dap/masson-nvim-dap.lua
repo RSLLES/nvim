02914ae61b46f2808e8d24bc507167046238d9e0
Romain Seailles <romain.seailles@inria.fr>
2024-10-30 20:14:35 +0100

    Test to fix debugger timeout

--- lua/plugins/dap/masson-nvim-dap.lua
+++ lua/plugins/dap/masson-nvim-dap.lua
@@ -1,19 +1,25 @@
 return {
-  "jay-babu/mason-nvim-dap.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap",
-  },
-  events = "VeryLazy",
-  config = function ()
-    local mason_nvim_dap = require("mason-nvim-dap")
+	"jay-babu/mason-nvim-dap.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"mfussenegger/nvim-dap",
+	},
+	events = "VeryLazy",
+	config = function()
+		local mason_nvim_dap = require("mason-nvim-dap")
 
-    mason_nvim_dap.setup({
-      ensure_installed = {
-        "codelldb", -- c++
-        "python", -- install debugpy for python
-      },
-      handlers = {},
-    })
-  end
+		mason_nvim_dap.setup({
+			ensure_installed = {
+				"codelldb", -- c++
+				"python", -- install debugpy for python
+			},
+			handlers = {
+				-- default handler
+				function(config)
+					config.adapters.initialize_timeout_sec = 10
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
+		})
+	end,
 }
