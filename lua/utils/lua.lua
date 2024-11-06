aeb18d2e306acabab035bc0b93c7e20dd0aa6bd2
RSLLES <rslles@abc.com>
2024-11-06 09:11:50 +0100

    Support windows path and python module

--- lua/utils/lua.lua
+++ lua/utils/lua.lua
@@ -5,4 +5,9 @@ function M.escape_pattern(text)
 	return text:gsub("([%-%.%(%)%+%*%?%[%^%$])", "%%%1")
 end
 
+--- Returns true if the operating system is Windows.
+function M.is_windows()
+	return package.config:sub(1, 1) == "\\"
+end
+
 return M
--- lua/utils/python.lua
+++ lua/utils/python.lua
@@ -2,9 +2,20 @@ local M = {}
 
 local lua_utils = require("utils.lua")
 
+function get_python_interpeter_path_windows()
+	local output = vim.fn.system("where python")
+	local first_match = output:match("^[^\r\n]+")
+	return first_match
+end
+
+function get_python_interpeter_path_unix()
+	return vim.fn.system("which python"):gsub("%s+", "") -- Trim whitespace
+end
+
 -- returns a python interpreter path
 function M.get_python_interpeter_path()
-	local system_python = vim.fn.system("which python"):gsub("%s+", "") -- Trim whitespace
+	local system_python = lua_utils.is_windows() and get_python_interpeter_path_windows()
+		or get_python_interpeter_path_unix()
 	if system_python == "" then
 		error("No Python interpreter found. Please ensure Python is installed and in your PATH.")
 	end
@@ -16,16 +27,15 @@ function M.get_module_path()
 	local file_path = vim.fn.expand("%:p")
 	local cwd = vim.fn.getcwd()
 	local cwd_escaped = lua_utils.escape_pattern(cwd)
-	-- vim.notify("Full current file path is " .. file_path)
-	-- vim.notify("Current working directory is " .. cwd)
 	if not file_path:find("^" .. cwd_escaped) then
 		error(file_path .. " is not within the current working directory: " .. cwd)
 	end
 	if not file_path:match("%.py$") then
 		error(file_path .. " does not have a .py extension")
 	end
-	local relative_path = file_path:sub(#cwd + 2) -- +2 to remove both `/` and the cwd
-	local module_path = relative_path:gsub("/", "."):gsub("%.py$", "")
+	local relative_path = file_path:sub(#cwd + 2) -- +2 to remove both separator and the cwd
+	local path_sep = lua_utils.is_windows() and "\\" or "/"
+	local module_path = relative_path:gsub(path_sep, "."):gsub("%.py$", "")
 	return module_path
 end
 
